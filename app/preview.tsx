c97632d9c83e3378ab6f22e99acbe83ecc616767
WU Yue <yuewuxd@gmail.com>
2025-02-25 11:29:56 +0800

    share as pdf now working

--- app/preview.tsx
+++ app/preview.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet, View, SafeAreaView, ImageBackground } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
+import * as Sharing from "expo-sharing";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "@/constants/Colors";
+import { type PageSize, PageSizes } from "@/constants/PageSizes";
 import { BackArrow } from "@/components/BackArrow";
 import { ImageSwiper } from "@/components/ImageSwiper";
 import { CustomButton } from "@/components/CustomButton";
-import * as Print from "expo-print";
-import * as Sharing from "expo-sharing";
-import { manipulateAsync } from "expo-image-manipulator";
-import { type PageSize, PageSizes } from "@/constants/PageSizes";
+import generatePdfFromImages from "@/utils/generatePdfFromImages";
 
 export default function PreviewScreen() {
   const params = useLocalSearchParams<{ images: string; pageSize: PageSize }>();
@@ -16,76 +15,9 @@ export default function PreviewScreen() {
   // Parse the stringified array of image URIs
   const images: string[] = params.images ? JSON.parse(params.images) : [];
 
-  const generatePdfForImages = async (uris: string[], pageSize: PageSize) => {
-    const images = await Promise.all(uris.map((uri) => manipulateAsync(uri, [], { base64: true })));
-
-    const html = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <style>
-            @page {
-              margin: 0;
-            }
-            * {
-              margin: 0;
-              padding: 0;
-              box-sizing: border-box;
-            }
-            body, html {
-              margin: 0;
-              padding: 0;
-              width: 100%;
-              height: 100%;
-            }
-            .page {
-              width: 100%;
-              height: 100%;
-              page-break-after: always;
-            }
-            .page:last-child {
-              page-break-after: avoid;
-            }
-            img {
-              display: block;
-              width: 100%;
-              height: 100%;
-              object-fit: contain;
-            }
-          </style>
-        </head>
-        <body>
-          ${images
-            .map(
-              (image) => `
-            <div class="page">
-              <img src="data:image/jpeg;base64,${image.base64}" />
-            </div>
-          `
-            )
-            .join("")}
-        </body>
-      </html>
-    `;
-
-    return Print.printToFileAsync({
-      html,
-      base64: false,
-      height: PageSizes[pageSize].height,
-      width: PageSizes[pageSize].width,
-      margins: {
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-      },
-    });
-  };
-
   const handleSharePDF = async () => {
     try {
-      const result = await generatePdfForImages(images, params.pageSize);
+      const result = await generatePdfFromImages(images, params.pageSize);
 
       await Sharing.shareAsync(result.uri, {
         mimeType: "application/pdf",
---
+++ utils/generatePdfFromImages.ts
@@ -0,0 +1,72 @@
+import * as Print from "expo-print";
+import { manipulateAsync } from "expo-image-manipulator";
+import { type PageSize, PageSizes } from "@/constants/PageSizes";
+
+const generatePdfFromImages = async (uris: string[], pageSize: PageSize) => {
+  const images = await Promise.all(uris.map((uri) => manipulateAsync(uri, [], { base64: true })));
+
+  const html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <style>
+            @page {
+              margin: 0;
+            }
+            * {
+              margin: 0;
+              padding: 0;
+              box-sizing: border-box;
+            }
+            body, html {
+              margin: 0;
+              padding: 0;
+              width: 100%;
+              height: 100%;
+            }
+            .page {
+              width: 100%;
+              height: 100%;
+              page-break-after: always;
+            }
+            .page:last-child {
+              page-break-after: avoid;
+            }
+            img {
+              display: block;
+              width: 100%;
+              height: 100%;
+              object-fit: contain;
+            }
+          </style>
+        </head>
+        <body>
+          ${images
+            .map(
+              (image) => `
+            <div class="page">
+              <img src="data:image/jpeg;base64,${image.base64}" />
+            </div>
+          `
+            )
+            .join("")}
+        </body>
+      </html>
+    `;
+
+  return Print.printToFileAsync({
+    html,
+    base64: false,
+    height: PageSizes[pageSize].height,
+    width: PageSizes[pageSize].width,
+    margins: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    },
+  });
+};
+
+export default generatePdfFromImages;
